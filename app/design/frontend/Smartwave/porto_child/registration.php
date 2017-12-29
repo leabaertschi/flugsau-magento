17926e36e52e03cbee2edf23ed2f5ee980aa3110
Lea Haensenberger <lea.haensenberger@gmail.com>
2017-12-29 18:15:20 +0100

    child theme

---
+++ app/design/frontend/Smartwave/porto_child/registration.php
@@ -0,0 +1,10 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+
+  \Magento\Framework\Component\ComponentRegistrar::THEME,
+
+  'frontend/Smartwave/port_child',
+
+  __DIR__
+);
